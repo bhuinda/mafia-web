3e41db93161c03486b9f35a419311699bfec13d2
bhuinda <bhuinda@sbcglobal.net>
2023-11-12 15:15:48 -0600

    Selector highlight logic finalized
    Workaround for applying CSS to innerHTML injections: https://onthecode.co.uk/blog/making-styles-work-with-innerhtml-in-angular

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
 
+export class HeaderComponent {
+  getSelectorHTML(page: string): string {
+    return `
+      <div class="selector">
+        [<span class="highlight">${page.toUpperCase()}</span>]
+      </div>
+    `;
+  }
 }
