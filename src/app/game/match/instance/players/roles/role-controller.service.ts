b3a1b263d0ea22d4b93f149f57a13e0dec2562da
bhuinda <bhuinda@sbcglobal.net>
2023-12-04 17:16:00 -0600

    Game experiment: attempt at natively fetching JSON data
    Experimentation has led me to realize that I *require* a backend for even basic testing. Will begin working with Firebase just to try it out

--- src/app/game/match/instance/players/roles/role-controller.service.ts
+++ src/app/game/match/instance/players/roles/role-controller.service.ts
@@ -1,36 +1,52 @@
-import { Injectable } from "@angular/core";
+import { Injectable, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 
 import { Role } from "./role.model";
 import { RoleProfile } from "./role-profile.model";
 import { RoleOptions } from "./role-options.model";
-import { Observable } from "rxjs";
+import { Metadata } from "../../metadata.service";
 
 @Injectable()
-export class RoleController {
+export class RoleController implements OnInit {
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private metadata: Metadata
+    ) {}
 
-  createRole(id: string, locale: string): Observable<Role> {
-    return this.http.get(`./path-to-data/${id}/${locale}.json`)
-      .pipe(
-        map(data => {
-          const roleProfile: RoleProfile = {
-            name: data.name,
-            description: data.description,
-            alignment: data.alignment,
-            faction: data.faction,
-            ability: data.ability,
-            traits: data.traits
-          };
-
-          const roleOptions: RoleOptions = {
-            traits: data.traits
-          };
-
-          return new CitizenRole(roleProfile, roleOptions);
-        })
-    );
+  ngOnInit(): void {
+    console.log(this.metadata.catalog);
   }
+
+  // createRole(id: string, locale: string): Observable<Role> {
+  //   return this.http.get(`./data/${id}/${locale}.json`)
+  //     .pipe(
+  //       map((data: any) => {
+  //         const roleProfile: RoleProfile = {
+  //           name: data.name,
+  //           description: data.description,
+  //           alignment: data.alignment,
+  //           faction: data.faction,
+  //           ability: data.ability,
+  //           traits: data.traits
+  //         };
+
+  //         const roleOptions: RoleOptions = {
+  //           traits: data.traits
+  //         };
+
+  //         const role = this.metadata.catalog['TownRoles'];
+  //         if (!role) {
+  //           throw new Error(`Invalid role ID: ${id}`);
+  //         }
+
+  //         return {
+  //           roleProfile,
+  //           roleOptions
+  //         }
+  //       })
+  //   );
+  // }
 }
