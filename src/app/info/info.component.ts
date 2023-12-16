891ca85dec94f67487cce3346dbfa30080fe3a0f
bhuinda <bhuinda@sbcglobal.net>
2023-12-16 00:02:12 -0600

    Added toggling prompt to InfoComponent home view

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./info.component.css']
 })
 export class InfoComponent {
+  isComponentActive = false;
 
+  onActivate() {
+    this.isComponentActive = true;
+  }
+
+  onDeactivate() {
+    this.isComponentActive = false;
+  }
 }
