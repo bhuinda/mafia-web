7b9b37056f1185bad0a667a80738fc1b3fd3f743
bhuinda <bhuinda@sbcglobal.net>
2023-12-27 07:57:53 -0600

    A most incredible addition
    /bhuinda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { SettingsService } from './shared/settings.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  settings = inject(SettingsService);
+
+  secretModeSubscription: Subscription;
+  secretMode: boolean;
+
   title = 'hi';
+
+  ngOnInit(): void {
+    this.secretModeSubscription = this.settings.secretMode$.subscribe(mode => {
+        this.secretMode = mode;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.secretModeSubscription.unsubscribe();
+  }
 }
