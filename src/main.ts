9acf8fa1b8f6eac8c8940d6b6dc9bb259c037a6c
bhuinda <bhuinda@sbcglobal.net>
2024-02-19 18:29:31 -0600

    Removed use of ngModule from app
    Moved all route info into main.ts, then abstracted config data from main.ts into its own app/config.ts file.
    
    File structure is much cleaner now!

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,6 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
-import { importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app/app-routing.module';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { AppConfig } from './app/app.config';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCkqYBq3krhnhc50U6IdECblCV9tnNJyzw",
-  authDomain: "ng-mafia4web.firebaseapp.com",
-  databaseURL: "https://ng-mafia4web-default-rtdb.firebaseio.com",
-  projectId: "ng-mafia4web",
-  storageBucket: "ng-mafia4web.appspot.com",
-  messagingSenderId: "1065272022680",
-  appId: "1:1065272022680:web:0ff57c33700cf5dd55e2ed",
-  measurementId: "G-48X3FQG9EH"
-};
-
-bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(provideFirebaseApp(() => initializeApp(firebaseConfig)), provideFirestore(() => getFirestore()), provideAuth(() => getAuth()), BrowserModule, AppRoutingModule, ReactiveFormsModule),
-        provideHttpClient(withInterceptorsFromDi())
-    ]
-})
+bootstrapApplication(AppComponent, AppConfig)
   .catch(err => console.error(err));
