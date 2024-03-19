82887da526960fbe3bd858c3d0426bb83b1774fd
bhuinda <bhuinda@sbcglobal.net>
2024-03-19 03:12:27 -0500

    Dropdown menu bugfix
    Implemented a solution to avoid weird flickering views upon modal close by making the modal closure async; modal now closes AFTER router navigation has finished.

--- src/app/components/header/dropdown/dropdown.component.ts
+++ src/app/components/header/dropdown/dropdown.component.ts
@@ -1,6 +1,7 @@
 import { NgClass } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { NavigationCancel, NavigationEnd, Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'dropdown',
@@ -10,11 +11,17 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './dropdown.component.scss'
 })
 export class DropdownComponent {
+  router = inject(Router);
+
   @Input() visible = false;
   @Output() closed = new EventEmitter<void>();
 
   closeModal() {
-    this.visible = false;
-    this.closed.emit();
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd || event instanceof NavigationCancel)
+    ).subscribe(() => {
+      this.visible = false;
+      this.closed.emit();
+    });
   }
 }
