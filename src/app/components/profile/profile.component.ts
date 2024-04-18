48eec8c0b8c8031bd8a380b0a9707eebb3cea15a
bhuinda <bhuinda@sbcglobal.net>
2024-04-18 09:07:31 -0500

    Overhauled ProfileComponent

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { User } from '@app/shared/models/user';
+import { UserService } from '@app/shared/services/user';
+import { UserInfoService } from '@app/shared/services/user-info';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-profile',
     templateUrl: './profile.component.html',
     styleUrls: ['./profile.component.scss'],
+    imports: [DatePipe],
     standalone: true
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit, OnDestroy {
+  math = Math;
 
+  user: User;
+  userService = inject(UserService);
+  userSubscription: Subscription;
+
+  userInfo: any;
+  userInfoService = inject(UserInfoService);
+  userInfoSubscription: Subscription;
+
+  ngOnInit(): void {
+    this.userSubscription = this.userService.user$.subscribe(user => this.user = user);
+    this.userInfoSubscription = this.userInfoService.userInfo$.subscribe(userInfo => this.userInfo = userInfo);
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+    this.userInfoSubscription.unsubscribe();
+  }
 }
